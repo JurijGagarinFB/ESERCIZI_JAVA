e7f9c3745d67cba99ea4862da96c00fd9c3c0c62
JurijGagarinFB <150333738+JurijGagarinFB@users.noreply.github.com>
2024-04-10 11:02:10 +0200

    Update Persona.java

--- Rubrica/src/Persona.java
+++ Rubrica/src/Persona.java
@@ -11,4 +11,7 @@ public class Persona {
     public String anagrafica() {
         return String.format("Nome: %s Cognome: %s Telefono: %s Tipo: %s", nome, cognome, numDiTelefono, tipo);
     }
+    public String ricercaSingola() {
+        return String.format("Telefono: %s", numDiTelefono);
+    }
 }
