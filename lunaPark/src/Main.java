dd6fd4243374bff9db9172b9088f5b48573c935c
JurijGagarinFB <150333738+JurijGagarinFB@users.noreply.github.com>
2024-11-16 08:37:51 +0100

    Update Main.java

--- lunaPark/src/Main.java
+++ lunaPark/src/Main.java
@@ -2,8 +2,6 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import static Scuderia.src.Tools.Utility.*;
-
 public class Main {
     public static void main(String[] args) {
         boolean esci = false;
