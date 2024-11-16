ea490de9e399e93ce45b284d8c1977a82d9ea5a5
JurijGagarinFB <francesco.bazaj@iisviolamarchesini.edu.it>
2024-11-16 09:22:43 +0100

    Update GaraScuderia

--- GaraScuderia/src/Gara.java
+++ GaraScuderia/src/Gara.java
@@ -1,12 +1,40 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Gara {
     private ArrayList<Auto> auto;
-}
-public Gara (ArrayList<Auto> auto){
 
-}
+    public Gara(ArrayList<Auto> auto) {
+        setAuto(auto);
+    }
 
-public void setAuto(){
-    this.auto = auto;
+    public void setAuto(ArrayList<Auto> auto) {
+        this.auto = auto;
+    }
+
+    public ArrayList<Auto> getAuto() {
+        return auto;
+    }
+
+    public ArrayList<Auto> randomizeAuto() {
+        Random random = new Random();
+        ArrayList<Auto> copiaAuto = auto;
+        int indice = 0; // Indice per il nuovo array
+        boolean presente;
+        int finale = 0;
+        do {
+            int numeroPosizione = random.nextInt(0, auto.size()) + 1;
+            presente = false;
+            // Controlla se il nuovo numero è già presente nel nuovo array
+            for (int j = 0; j < copiaAuto.size(); j++) {
+                if (copiaAuto.get(j) == auto.get(numeroPosizione)) {
+                    presente = true; // Se il numero è già presente, lo segnala
+                }
+            }
+            finale = numeroPosizione;
+            indice++;
+        } while (presente); // Continua finché il numero è già presente
+        copiaAuto.add(indice, auto.get(finale)); // Aggiunge il nuovo numero
+        return copiaAuto;
+    }
 }
