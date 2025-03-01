c1d4bcf4c1a87deb1bcc33cce5186f2e4d76417e
JurijGagarinFB <francesco.bazaj@iisviolamarchesini.edu.it>
2025-03-01 09:14:40 +0100

    Update DadoAdOggetti (aggiunta partitaAvanzata sbagliata)

--- IV_2024_2025/DadoAdOggetti/src/Gara.java
+++ IV_2024_2025/DadoAdOggetti/src/Gara.java
@@ -32,25 +32,46 @@ public class Gara {
         }
     }
 
-    public String round() {
+    public String round(boolean partitaBase) {
         if (fineGara) {
             return "La gara è già finita.";
         }
+        int conta1 = 0, conta2 = 0;
         int lancio1 = dado.lancia();
         int lancio2 = dado.lancia();
 
-        String risultato = "-----------------------\n" +
-                giocatore1.getNome() + " ha lanciato: " + lancio1 + "\n" + dado.stampaFacce(lancio1) + "\n" +
-                giocatore2.getNome() + " ha lanciato: " + lancio2 + "\n" + dado.stampaFacce(lancio2);
-
         if (lancio1 > lancio2) {
             giocatore1.incrementaVittorie();
+            if (!partitaBase) {
+                conta1++;
+                conta2 = 0;
+                if (conta1 == 3) {
+                    giocatore1.incrementaVittorie();
+                    conta1 = 0;
+                }
+            }
         } else if (lancio2 > lancio1) {
             giocatore2.incrementaVittorie();
+            if (!partitaBase) {
+                conta2++;
+                conta1 = 0;
+                if (conta2 >= 3) {
+                    giocatore1.incrementaVittorie();
+                    conta2 = 0;
+                }
+            }
         } else {
             giocatore1.incrementaVittorie();
             giocatore2.incrementaVittorie();
+            conta1 = 0;
+            conta2 = 0;
         }
+
+        String risultato = "-----------------------\n" +
+                giocatore1.getNome() + " ha lanciato: " + lancio1 + "\n" + dado.stampaFacce(lancio1) + "\n" +
+                giocatore2.getNome() + " ha lanciato: " + lancio2 + "\n" + dado.stampaFacce(lancio2) + "\n" +
+                giocatore1.getNome() + " è a: " + giocatore1.getVittorie() + " vittorie con una streak di : " + conta1 + "\n" +
+                giocatore2.getNome() + " è a: " + giocatore2.getVittorie() + " vittorie con una streak di : " + conta2 + "\n";
 
         roundCorrente++;
         if (roundCorrente >= roundTotali) {
--- IV_2024_2025/DadoAdOggetti/src/Main.java
+++ IV_2024_2025/DadoAdOggetti/src/Main.java
@@ -1,12 +1,40 @@
+/*
+modifica la classe partita in modo che:
+    -sia possibile gicare sia le partite base che le partite avanzate
+    -la partita base funziona come nella vecchia versione
+    -la partita avanzata aggiunge un sistema di bonus:
+        -tiene traccia delle vittorie consecutive. Dopo 3 vittorie consecutive, assegna un punto (una vincita) bonus al giocatore.
+        -il bonus viene azzerato in caso di pareggio.
+
+    -Si consideri di implementare il polimorfismo implementando nel main un metodo che permette di giocare
+    una partita standard o una partita avanzata
+    giocaPartita(partitaBase);
+    giocaPartita(partitaAvanzata);
+*/
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        boolean partitaBase = true, inserimentoCorretto;
+        do {
+            inserimentoCorretto = true;
+            System.out.println("Inserire tipo di partita (Base o Avanzata): ");
+            if (sc.nextLine().equalsIgnoreCase("base")) {
+                partitaBase = true;
+            } else if (sc.nextLine().equalsIgnoreCase("avanzata")) {
+                partitaBase = false;
+            } else {
+                inserimentoCorretto = false;
+            }
+        } while (!inserimentoCorretto);
+
         System.out.println("Inserire nome Giocatore 1: ");
         String nome1 = sc.nextLine();
         System.out.println("Inserire nome Giocatore 2: ");
         String nome2 = sc.nextLine();
+
 
         int numeroRound;
         int numeroFacce;
@@ -21,7 +49,7 @@ public class Main {
 
         Gara gara = new Gara(nome1, nome2, numeroRound, new Dado(numeroFacce));
         while (!gara.isFineGara()) {
-            System.out.println(gara.round());
+            System.out.println(gara.round(partitaBase));
             attesa(2);
         }
         System.out.println(gara.getVincitore());
